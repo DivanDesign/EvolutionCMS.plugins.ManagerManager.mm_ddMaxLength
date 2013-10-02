9075ee02d0498f81f07cf740f28be8068db09e9f
Ilyas Ronef <ilyas@divandesign.biz>
2013-10-02 19:17:08 +0600

    Prerelease 1.1

--- ddmaxlength/ddmaxlength.php
+++ ddmaxlength/ddmaxlength.php
@@ -1,8 +1,8 @@
 <?php
 /**
  * mm_ddMaxLength
- * @version 1.0.1 (2012-01-13)
- *
+ * @version 1.1 (2013-10-02)
+ * 
  * Widget for ManagerManager plugin allowing number limitation of chars inputing in fields (or TVs).
  * 
  * @uses ManagerManager plugin 0.5.2.
@@ -12,29 +12,29 @@
  * @param $templates {comma separated string} - Id of the templates to which this widget is applied. Default: ''.
  * @param $length {integer} - Maximum number of inputing chars. Default: 150.
  * 
- * @link http://code.divandesign.biz/modx/mm_ddmaxlength/1.0.1
- *
- * @copyright 2012, DivanDesign
+ * @link http://code.divandesign.biz/modx/mm_ddmaxlength/1.1
+ * 
+ * @copyright 2013, DivanDesign
  * http://www.DivanDesign.biz
  */
 
 function mm_ddMaxLength($fields = '', $roles = '', $templates = '', $length = 150){
 	global $modx, $mm_current_page, $mm_fields;
 	$e = &$modx->Event;
-
+	
 	if ($e->name == 'OnDocFormRender' && useThisRule($roles, $templates)){
 		$output = '';
 		
 		$fields = getTplMatchedFields($fields, 'text,textarea');
 		if ($fields == false){return;}
-
+		
 		$site = $modx->config['site_url'];
-
+		
 		$output .= "// ---------------- mm_ddMaxLength :: Begin ------------- \n";
 		//General functions
 		$output .= includeJs($site.'assets/plugins/managermanager/js/jquery.ddTools-1.8.1.min.js', 'js', 'jquery.ddTools', '1.8.1');
 		$output .= includeCss($site.'assets/plugins/managermanager/widgets/ddmaxlength/ddmaxlength.css');
-
+		
 		foreach ($fields as $field){
 			$output .= '
 $j("'.$mm_fields[$field]['fieldtype'].'[name='.$mm_fields[$field]['fieldname'].']").addClass("ddMaxLengthField").each(function(){
@@ -46,7 +46,7 @@ $j("'.$mm_fields[$field]['fieldtype'].'[name='.$mm_fields[$field]['fieldname'].'
 });
 			';
 		}
-
+		
 		$output .= '
 $j("#mutate").submit(function(){
 	var ddErrors = new Array();
@@ -59,19 +59,19 @@ $j("#mutate").submit(function(){
 			ddErrors.push(field.parents("tr").find("td:first-child .warning").text());
 		}
 	});
-
+	
 	if(ddErrors.length > 0){
 		alert("Некорректно заполнены поля: " + ddErrors.join(","));
 		
 		return false;
-	} else {
+	}else{
 		return true;
 	}
 });
 		';
-
+		
 		$output .= "\n// ---------------- mm_ddMaxLength :: End -------------";
-
+		
 		$e->output($output . "\n");
 	}
 }
